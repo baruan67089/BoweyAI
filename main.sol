c98b961968e45ae456bedb9551ad185dda3f64ea
baruan67089 <baruan67089@gmail.com>
2026-03-30 12:35:19 +0200

    Update main.sol

--- main.sol
+++ main.sol
@@ -76,3 +76,81 @@ library BoweyAI_Strings {
 
     function toHexString(uint256 value, uint256 length) internal pure returns (string memory) {
         bytes memory buffer = new bytes(2 * length + 2);
+        buffer[0] = "0";
+        buffer[1] = "x";
+        for (uint256 i = 2 * length + 1; i > 1; --i) {
+            buffer[i] = _HEX[value & 0xf];
+            value >>= 4;
+        }
+        return string(buffer);
+    }
+
+    function toHexAddress(address a) internal pure returns (string memory) {
+        return toHexString(uint256(uint160(a)), 20);
+    }
+}
+
+library BoweyAI_ECDSA {
+    error BoweyAI_ECDSA_BadSignature();
+    error BoweyAI_ECDSA_BadS();
+    error BoweyAI_ECDSA_BadV();
+
+    function recover(bytes32 digest, bytes memory sig) internal pure returns (address) {
+        if (sig.length != 65) revert BoweyAI_ECDSA_BadSignature();
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        assembly {
+            r := mload(add(sig, 0x20))
+            s := mload(add(sig, 0x40))
+            v := byte(0, mload(add(sig, 0x60)))
+        }
+        return recover(digest, v, r, s);
+    }
+
+    function recover(bytes32 digest, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        if (uint256(s) > 0x7fffffffffffffffffffffffffffffff5d576e7357a4501ddfe92f46681b20a0) {
+            revert BoweyAI_ECDSA_BadS();
+        }
+        if (v != 27 && v != 28) revert BoweyAI_ECDSA_BadV();
+        address signer = ecrecover(digest, v, r, s);
+        if (signer == address(0)) revert BoweyAI_ECDSA_BadSignature();
+        return signer;
+    }
+
+    function toEthSignedMessageHash(bytes32 h) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", h));
+    }
+
+    function toTypedDataHash(bytes32 domainSeparator, bytes32 structHash) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
+    }
+}
+
+library BoweyAI_Merkle {
+    function verify(bytes32[] memory proof, bytes32 root, bytes32 leaf) internal pure returns (bool) {
+        bytes32 h = leaf;
+        for (uint256 i = 0; i < proof.length; ++i) {
+            bytes32 p = proof[i];
+            h = h < p ? keccak256(abi.encodePacked(h, p)) : keccak256(abi.encodePacked(p, h));
+        }
+        return h == root;
+    }
+}
+
+contract BoweyAI is BoweyAI_ReentrancyGuard, BoweyAI_Pausable {
+    using BoweyAI_SafeTransfer for IERC20Minimal;
+    using BoweyAI_Strings for uint256;
+    using BoweyAI_Strings for address;
+
+    // ----- errors -----
+    error BoweyAI_NotOwner();
+    error BoweyAI_NotPendingOwner();
+    error BoweyAI_NotAuthorized();
+    error BoweyAI_BadInput();
+    error BoweyAI_TooLong();
+    error BoweyAI_RateLimited(uint256 nextAt);
+    error BoweyAI_InsufficientBalance();
+    error BoweyAI_AlreadyExists();
+    error BoweyAI_NotFound();
+    error BoweyAI_ZeroAmount();
